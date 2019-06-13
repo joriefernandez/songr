e8431fd9dc5efbcd376a65fbe361938d124f28c0
Jorie Fernandez <joriefefernandez@yahoo.com>
2019-06-13 12:41:29 -0700

    Updated comments

--- src/main/java/com/fern/jorie/projectlab/songr/Album.java
+++ src/main/java/com/fern/jorie/projectlab/songr/Album.java
@@ -5,6 +5,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+/**
+ * Album class that contains album info. Ensure to create getters and setters.
+ */
+
 @Entity
 public class Album {
     @Id
