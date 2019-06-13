f23a80eb04aeacd0922976752edd74fe98312dcd
Jorie Fernandez <joriefefernandez@yahoo.com>
2019-06-13 09:32:05 -0700

    Added album class

---
+++ src/main/java/com/fern/jorie/projectlab/songr/Album.java
@@ -0,0 +1,56 @@
+package com.fern.jorie.projectlab.songr;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Album {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+    String title;
+    String artist;
+    int songCount;
+    long length;
+    String imageUrl;
+
+    //Constructors
+
+    public Album(){}
+
+    public Album(long id, String title, String artist, int songCount, long length, String imageUrl) {
+        this.id = id;
+        this.title = title;
+        this.artist = artist;
+        this.songCount = songCount;
+        this.length = length;
+        this.imageUrl = imageUrl;
+    }
+
+    //Getters
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public int getSongCount() {
+        return songCount;
+    }
+
+    public long getLength() {
+        return length;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+}
